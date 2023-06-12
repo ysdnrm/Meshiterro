1ca6c0d9836e4b43f4aa738ae257e4a30f1c0000
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-12 12:00:06 +0000

    [Finish] 4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # devise のモデルを作成したことでルーティングが自動的に追加
+  # devise を使用する際に URL として users を含むことを示している
+  devise_for :users
+  # Topページに[root_path]を設定
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
