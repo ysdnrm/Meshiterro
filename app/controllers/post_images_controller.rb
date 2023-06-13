77a2d286893b8e4492f283c62e551bd5a3513284
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-13 10:37:50 +0000

    [Finish] 4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,14 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image = PostImage.new
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    # current_user は、ログイン中のユーザーの情報を取得できる便利な記述(devise を導入しないと使用することができない)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -7,4 +16,12 @@ class PostImagesController < ApplicationController
 
   def show
   end
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+  
 end
