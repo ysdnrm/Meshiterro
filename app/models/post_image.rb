87b1c80d35991ce59427ef7d312c840a9cfda6d5
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-13 09:36:30 +0000

    [Finish] 4-9

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,8 @@
 class PostImage < ApplicationRecord
-  
   has_one_attached :image
+
+  # アソシエーション（Userに属するということ）
+  belongs_to :user
+    # PostImage モデルに関連付けられるのは、1 つの User モデル
+    # このため、単数形の「user」になっている点に注意
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  #  ⇩アソシエーション ,   ⇩ユーザーIDを消したら投稿も消える
+  has_many :post_images, dependent: :destroy
 end
