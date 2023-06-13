6c55cca6ee7c9d03d8d552d92b66b5c82df9078b
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-13 05:54:46 +0000

    [finish] 4-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   # deviseのコントローラは直接修正できないため、全てのコントローラに対する処理を行える権限を持つ、ApplicationControllerに
   # deviseのストロングパラメータを編集を記述する必要があります。
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  # aboutページをサインイン後の遷移先に設定
+   def after_sign_in_path_for(resource)
+     about_path
+   end
+   
+  # aboutページをサインアウト後の遷移先に設定
+   def after_sign_out_path_for(resource)
+     about_path
+   end
 
 # privateは記述をしたコントローラ内でしか参照できません。
 # 一方、protectedは呼び出された他のコントローラからも参照することができます。
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Topページに[root_path]を設定
   root to: "homes#top"
+
+  get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
