634cc063e20b7ea0f2e3bd61ef0571fe12003667
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-13 05:14:46 +0000

    [Finish] 4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  # deviseのコントローラは直接修正できないため、全てのコントローラに対する処理を行える権限を持つ、ApplicationControllerに
+  # deviseのストロングパラメータを編集を記述する必要があります。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+# privateは記述をしたコントローラ内でしか参照できません。
+# 一方、protectedは呼び出された他のコントローラからも参照することができます。
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # ストロングパラメータと同様の機能↑
+  end
 end
