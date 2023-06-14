0c3a7114726d19197b457bc1e82f398bfb793343
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-14 11:17:01 +0000

    [Finish] 4-17

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
    
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
