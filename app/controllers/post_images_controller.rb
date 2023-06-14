87e25aeb14f5bd3a8821b9d04e80e7df0f3272e3
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-14 10:05:24 +0000

    [Finish] 4-15

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -14,7 +14,7 @@ class PostImagesController < ApplicationController
   def index
     @post_images = PostImage.all
   end
-
+  
   def show
     @post_image = PostImage.find(params[:id])
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
+  
   def show
+    @user = User.find(params[:id])
+    
+  # @user.post_imagesに格納されているデータは複数あるため,
+  # 関連づけられているモデルのカラムを操作することはできません。
+    @post_images = @user.post_images
   end
 
   def edit
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
