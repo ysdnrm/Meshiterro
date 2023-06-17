7369e68f18c4e130402b2f61189f2646bde990a5
ysdnrm <133837502+ysdnrm@users.noreply.github.com>
2023-06-17 14:27:33 +0000

    [Finish] 4-26

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # 各アクションを実行する前に実行したい処理を指定(before_action)
+    # メソッドは一番下に定義してる
+  before_action :is_matching_login_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -23,6 +26,14 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+  
+  # 他のユーザーからのアクセス制限(is_matching_login_userというメソッドにまとめる)
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
   
 end
